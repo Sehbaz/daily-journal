554bf0f351f63570e17dcbebebd13f9f8aa0f6c2
Sehbaz Rafik <sehbazrafik@Sehbazs-MacBook-Pro.local>
2019-09-15 00:14:16 +0100

    files update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const loDash=require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -11,6 +12,7 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+const posts = [];
 const app = express();
 
 app.set("view engine", "ejs");
@@ -19,9 +21,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { myP: homeStartingContent });
+  res.render("home", { startingContent: homeStartingContent,
+  posts:posts
+  });
 });
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactData: contactContent });
+});
+app.get("/about", (req, res) => {
+  res.render("about", { aboutData: aboutContent });
+});
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+app.post("/compose", (req, res) => {
+  const post = { title: req.body.postTitle, body: req.body.postBody };
+  posts.push(post);
+  res.redirect("/");
+});
+app.get('/posts/:postName', function (req, res) {
+  const requestedTitle=loDash.lowerCase(req.params.postName)
+  posts.forEach((post)=>{
+    if(requestedTitle===post.title){
+     res.render("post",{postTitle:post.title,postBody:post.body});
+    }
+  })
+})
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
