e49eaa2dabdba9237a4aba199278b09a0429dad3
Sehbaz Rafik <sehbazrafik@Sehbazs-MacBook-Pro.local>
2019-09-28 23:20:43 +0100

    database connection added

--- app.js
+++ app.js
@@ -3,51 +3,91 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const loDash=require("lodash");
+const loDash = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+mongoose.connect("mongodb+srv://sehbaz123:helloworld@devconnector-97frp.mongodb.net/myblogDB?retryWrites=true&w=majority/myblogDB", {
+  useNewUrlParser: true
+});
+const itemsSchema = {
+  title: String,
+  body: String
+};
+const Item = mongoose.model("Item", itemsSchema);
+
+
+
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
+
 const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { startingContent: homeStartingContent,
-  posts:posts
+  Item.find({}, (err, foundItems) => {
+    res.render("home", {
+      posts: foundItems
+    });
   });
 });
 app.get("/contact", (req, res) => {
-  res.render("contact", { contactData: contactContent });
+  res.render("contact", {
+    contactData: contactContent
+  });
 });
 app.get("/about", (req, res) => {
-  res.render("about", { aboutData: aboutContent });
+  res.render("about", {
+    aboutData: aboutContent
+  });
 });
 app.get("/compose", (req, res) => {
   res.render("compose");
 });
 app.post("/compose", (req, res) => {
-  const post = { title: req.body.postTitle, body: req.body.postBody };
-  posts.push(post);
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  };
+  const item = new Item({
+    title: post.title,
+    body: post.body
+  });
+  item.save();
   res.redirect("/");
 });
 app.get('/posts/:postName', function (req, res) {
-  const requestedTitle=loDash.lowerCase(req.params.postName)
-  posts.forEach((post)=>{
-    if(requestedTitle===post.title){
-     res.render("post",{postTitle:post.title,postBody:post.body});
-    }
-  })
-})
+  const requestedTitle = req.params.postName;
+  Item.findById(requestedTitle, function (err, coolPost) {
+    res.render("post", {
+      postTitle: coolPost.title,
+      postBody: coolPost.body
+    });
+  });
+  console.log(requestedTitle);
 
+});
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndDelete(checkedItemId, (err) => {
+    console.log(err);
+  })
+  res.redirect("/");
+});
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
+/*
+ res.render("post", {
+        postTitle: post.title,
+        postBody: post.body
+      });
+      */
